e80f76798c5da9d1f5632102350c19ef5b009f78
Md Rubel Hamza <rubel_xpress@yahoo.com>
2023-09-30 17:03:13 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { DateRange } from "react-date-range";
 
 function App() {
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: "selection",
+    },
+  ]);
+  const [selectorShown, setSelectorShown] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="App"
+      style={{ backgroundColor: "#dfdfe5", minHeight: "100vh" }}
+    >
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "33fr 33fr 33fr",
+          width: "100%",
+          gap: "10px",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "red",
+            height: "100px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            fontSize: "28px",
+            fontWeight: "bold",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Lets check
+        </div>
+        <div
+          style={{
+            backgroundColor: "#fefefe",
+            position: "relative",
+            padding: "10px",
+          }}
+        >
+          <div>Date Range: </div>
+          <div
+            style={{
+              color: "black",
+              border: "1px solid #ddd",
+              padding: "8px 12px",
+              borderRadius: "4px",
+            }}
+            onClick={() => {
+              setSelectorShown(true);
+            }}
+          >
+            <div>
+              {state[0].startDate.toLocaleDateString()}-
+              {state[0].endDate
+                ? state[0].endDate?.toLocaleDateString()
+                : state[0].startDate?.toLocaleDateString()}
+            </div>
+          </div>
+          {selectorShown && (
+            <div style={{ top: "68px", left: "10px", position: "absolute" }}>
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => {
+                  if (item.selection.endDate !== item.selection.startDate) {
+                    setSelectorShown(false);
+                  }
+                  setState([item.selection]);
+                }}
+                moveRangeOnFirstSelection={false}
+                ranges={state}
+              />
+            </div>
+          )}
+        </div>
+        <div
+          style={{
+            backgroundColor: "green",
+            height: "100px",
+            color: "white",
+            fontSize: "20px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            {state[0].startDate.toLocaleDateString()}-
+            {state[0].endDate
+              ? state[0].endDate?.toLocaleDateString()
+              : state[0].startDate?.toLocaleDateString()}
+          </div>
+        </div>
+        <div
+          style={{
+            backgroundColor: "purple",
+            height: "300px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            fontSize: "28px",
+            fontWeight: "bold",
+          }}
+        >
+          {state[0].startDate.toLocaleDateString()}
+        </div>
+        <div
+          style={{
+            backgroundColor: "crimson",
+            height: "300px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            fontSize: "28px",
+            fontWeight: "bold",
+          }}
+        >
+          {state[0].endDate?.toLocaleDateString()}
+        </div>
+        <div
+          style={{
+            backgroundColor: "pink",
+            height: "300px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            {state[0].startDate.toDateString()} {" -  "}
+            {state[0].endDate
+              ? state[0].endDate?.toDateString()
+              : state[0].startDate?.toDateString()}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
